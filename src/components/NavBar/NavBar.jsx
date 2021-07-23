92df8ae6946a8a41d213f23a469c8753cf1a72fc
Judy Kim <judykim@Judys-MacBook-Pro.local>
2021-07-23 16:25:51 -0700

    part 3 half way done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,11 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <nav>
-         <Link to="/puppies">Puppies History</Link>
-         &nbsp; | &nbsp;
-         <Link to="/puppies/new">New Puppy</Link>
-        </nav>
-    )
+  return<nav>
+    <Link to="/puppies">Puppies History</Link>
+    &nbsp; | &nbsp;
+    <Link to="/puppies/new">New Puppy</Link>
+  </nav>;
 }
 
 export default NavBar;
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,10 @@
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
 function AuthPage() {
 	return (
 		<main>
 			<h1>AuthPage</h1>
+            <SignUpForm />
 		</main>
 	);
 }
