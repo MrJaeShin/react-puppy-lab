a9b8252c2cb7cd9a5c0b28d268c259a945d18d94
Judy Kim <judykim@Judys-MacBook-Pro.local>
2021-07-27 20:33:06 -0700

    login function route fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
 router.post('/', usersCtrl.create);
-router.post('/login', userCtrl.login);
+router.post('/login', usersCtrl.login);
 
 module.exports = router;
